fd26bcfafc2de048bb283f4424e76218d8eb0204
Siddhesh Mangela <sidthecool007@gmail.com>
2020-04-14 19:46:46 +0530

    part 2 step 4 complete

--- lib/main.dart
+++ lib/main.dart
@@ -24,13 +24,20 @@ class RandomWords extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>(); // Add this line.
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
+
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
       ),
     );
   }
